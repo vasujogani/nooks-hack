1a0e36aeb3b5d19fd050d5af80bf536fc5ab1f46
Vishnu Rajan Tejus <67448237+vishnurajantejus@users.noreply.github.com>
2021-10-30 13:01:10 -0700

    generateLink

--- backend/main.py
+++ backend/main.py
@@ -4,14 +4,11 @@
 import sys
 import os
 import time
+import string
+import random
 from opentok import OpenTok, MediaModes
-from videocall import logger_stream
+from .room import Room
 
-import logging
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-logger.addHandler(logger_stream)
 OPENTOK_APP_ID = "46908704"
 OPENTOK_APP_SECRET = "32ffe9a1f88466836e7fa790cdd4f89ab73bbef9"
 
@@ -20,6 +17,14 @@ opentok_sdk = OpenTok("46908704", "32ffe9a1f88466836e7fa790cdd4f89ab73bbef9")
 
 def generate_session():
     session = opentok_sdk.create_session(media_mode=MediaModes.routed)
-    # logger.info(f"generate_session: {session.session_id}")
     return session.session_id
 
+doc_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k = 32))
+
+room = Room(
+    created_at = time.time(),
+    link_id = doc_id,
+    session_id = generate_session()
+)
+
+await db.collection("rooms").document(doc_id).set(room.to_dict())
